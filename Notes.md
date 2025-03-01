daead6b12173bca9483004c6b5afc2f177c2dee3
ISRA ILYAS <israqureshi138@gmail.com>
2025-03-01 22:56:26 +0500

    completed framer motion tutorial

--- Notes.md
+++ Notes.md
@@ -1,12 +1,66 @@
-# React + Vite
+# Framer Motion
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+- Install Framer Motion
+    `npm i motion`
 
-Currently, two official plugins are available:
+- Import motion component
+     `import {motion} from 'motion/react'` 
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+- Make you element motion component 
+     ` <motion.div>Box</motion.div>`
 
-## Expanding the ESLint configuration
+- Attributes/Props
+     `animate={{ x:1000,y:50,scale:1.5 }}` 
+     `transition={{ duration:2,delay:1 }}`     
 
-If you are developing a production application, we recommend using TypeScript and enable type-aware lint rules. Check out the [TS template](https://github.com/vitejs/vite/tree/main/packages/create-vite/template-react-ts) to integrate TypeScript and [`typescript-eslint`](https://typescript-eslint.io) in your project.
+### What is Framer Motion?
+
+Framer motion is an open-source animation library specifically designed for React. It allows developers to create animations and interactions with minimal code. Framer Motion is built on the foundation of the Framer library and provides a declarative API for defining animations, making it easy to create complex motion designs. It is widely used for:
+
+- Creating smooth, fluid animations
+- Enhancing user experience
+- High performance
+
+### Is Framer Motion Good?
+
+Framer Motion is considered one of the best animation libraries for React due to its:
+
+- Ease of use
+- Powerful features
+- community support
+
+### When To Choose Framer Motion:
+
+- You are working primarily with React and want a library that integrates seamlessly with it.
+- You need to quickly implement animations with simple and declarative API.
+- Your project involves a lot of interactive animations, such as drag, hover, and top gestures.
+
+### When To Choose GSAP:
+
+- You need advanced animations capabilities and highly customized animations.
+- Your project is not limit to React, or you are sing multiple frameworks.
+- You require the use of GSAP's powerful plugins for tasks like scroll-based animatons or complete SVG animations.
+
+
+### Outline
+
+#### Introduction to framer motion
+
+- Overview of framer motion.
+- Key features and benefits.
+- Installing framer motion.
+- Basic project setup with react-vite.
+
+#### Basic Animations
+
+- Motion components ('motion.div',etc)
+- Animate props ('initial', 'animate', 'exit')
+- Simple Transitions
+- Creating and using variant.
+
+#### Advanced ANimations
+
+- Using keyframes
+- Drag animations
+- Hover and tap animations
+- 
--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,113 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import { motion, useScroll } from 'motion/react'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+const {scrollYProgress} = useScroll()
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='w-full bg-black text-center font-mono'>
+
+{/* Key Frame Animation */}
+      {/* <motion.div >
+        <motion.img 
+        initial={{
+          x:0,
+          y:0
+        }}
+        animate={{
+          x: [0,1000,1000,0,0],
+          y:[0,0,300,300,0],
+          rotate:[0,360,0,-360,0]
+        }}
+          transition={{
+            duration: 4,
+            delay: 1,
+            repeat: Infinity,
+            ease: 'anticipate'
+          }}
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXDE64dpw-ziSSWqJkv4LLFNZbEmjqmSiejw&s" alt="" />
+      </motion.div> */}
+
+      {/* <motion.div 
+     animate={{
+      x:1000,
+      rotate:360
+     }} 
+     transition={{
+      duration:3,
+      delay:2,
+     }}
+     className='h-[200px] w-[200px] bg-red-500 text-xl flex justify-center items-center'>
+      Box
+     </motion.div>
+
+     <motion.div animate={{
+      scale:1.5,
+      x:400
+     }}
+     transition={{
+      delay:3,
+      duration:3
+     }}
+      className='h-[200px] w-[200px] bg-red-500 text-xl flex justify-center items-center rounded-full'>
+      Circle
+     </motion.div> */}
+
+     {/* === Hover and tap */}
+     {/* <motion.div 
+     whileHover={{
+      background:'blue'
+     }}
+     whileTap={{
+      rotate:360
+     }}
+     transition={{
+      duration:1
+     }}
+     className='bg-red-500 w-[200px] h-[200px] border-2 border-black m-3'></motion.div> */}
+
+     {/* Drag */}
+     {/* <motion.div 
+     drag
+     whileDrag={{
+      scale:0.8
+     }}
+     dragConstraints={{
+      left:0,
+      top:0,
+      right:1000,
+      bottom:300
+     }}
+     dragDirectionLock="true"
+     whileHover={{
+      background:'blue'
+     }}
+     whileTap={{
+      
+     }}
+     transition={{
+     }}
+     className='bg-red-500 w-[200px] h-[200px] border-2 border-black m-3'></motion.div> */}
+
+     {/* Scrolling Anmation */}
+     
+     <motion.div 
+     style={{
+      scaleX:scrollYProgress
+     }}
+     className='origin-left rounded-full bg-red-500 w-full h-3 fixed top-0 '></motion.div>
+
+     <div className=' px-20 py-20  text-white font-monospace flex flex-col gap-[50px]'>
+      <h2 className='text-4xl font-bold mb-8'>Scrolling Animation</h2>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum sed corporis officia pariatur officiis? Et, repudiandae quas, fuga rerum iusto provident blanditiis architecto sint nam quod quae, laudantium tempora voluptates enim odit! Quo, reprehenderit. Quaerat saepe laborum error repudiandae ea quod, molestias aut, ipsa fuga excepturi doloremque quisquam deserunt, accusamus blanditiis animi ut magnam. Exercitationem accusamus ea deleniti quod reprehenderit. Deleniti vel accusantium atque quos voluptate delectus consectetur sapiente inventore obcaecati possimus, nulla tempore voluptatem! Ipsam illum aliquid nihil vel voluptatem ipsa iste, hic quam ex ut veniam deserunt dolore! Saepe facere, dolorum expedita esse hic at aliquam perferendis itaque.</p>
+      <br />
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis, explicabo. Ipsa quod vero soluta iure! Deserunt quidem nisi unde quibusdam enim nulla hic itaque fugit sint repellat iste, vero error nesciunt voluptatibus ad architecto ea autem laboriosam est odio facere alias ipsam minima quas! Nisi sit quo earum quaerat amet animi a! Cumque, reiciendis consectetur? Consequuntur ex eum velit quis eligendi. Ab obcaecati aut laboriosam? Aliquam fuga atque odit quasi, accusamus illo possimus perspiciatis corrupti aspernatur vel labore dolore eos veritatis nihil nesciunt impedit doloribus ab non ullam. Blanditiis libero unde nisi sequi alias, dolorum tempore, quidem odio corporis eligendi dicta. Debitis quibusdam ipsam perferendis velit corporis. Incidunt placeat suscipit ut sapiente aliquid vitae. Deserunt, tempora nisi! Dignissimos officia cum maxime debitis et facere illo aspernatur suscipit est aliquid fugit quam quas nisi fugiat commodi iure veritatis repudiandae corporis asperiores autem quis, nihil sunt? Voluptate rerum ducimus tempore minus, labore magnam nam veritatis facilis accusantium consequuntur quam totam eius iste, praesentium assumenda debitis a nesciunt recusandae cupiditate? Facilis repudiandae officiis dignissimos natus delectus. Laboriosam minima ducimus dolorem beatae veniam velit dolor! Nulla provident minima est a praesentium, consectetur consequatur ad dolores, perferendis numquam pariatur ab dolorem laborum sunt iusto hic.</p>
+      <br />
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quisquam hic architecto repudiandae in totam incidunt esse ipsum neque voluptatibus quam consequuntur necessitatibus recusandae repellendus quidem soluta, expedita fuga ratione facere amet! Dolore iure, quam harum ratione vero sapiente quia accusantium, numquam aut ab laudantium qui impedit sit! Ipsa, animi consectetur? Nihil ipsam impedit vitae minus inventore reprehenderit temporibus odio maxime eum recusandae dolor blanditiis, aliquid, assumenda corporis debitis accusamus asperiores, nostrum dicta ad. Repellat, beatae voluptatibus. Sunt consequuntur quas commodi nemo dolor perferendis esse iste exercitationem minus alias quo error tenetur vitae inventore iusto ab obcaecati labore beatae pariatur iure, repellendus necessitatibus. Modi voluptatem quisquam, labore harum id sed perspiciatis ipsum. Esse autem consequatur eius harum odit iusto, in illo enim modi, alias ipsam unde labore totam necessitatibus. Minus accusamus velit iusto vitae cupiditate! Doloribus maiores accusamus ullam cum provident corrupti natus. Quos obcaecati vero ex commodi voluptatibus dolorum, asperiores cumque explicabo debitis dolores iste! Provident molestias vero incidunt hic. Dolor voluptate, laudantium officiis aut in aliquam nihil vero possimus, dolores deserunt tenetur id ducimus soluta quae! Aliquid blanditiis, eos consequuntur laboriosam accusamus aut minima, non, quam nam nulla exercitationem veritatis eaque repellendus at rem ad aspernatur possimus molestias! Quod!</p>
+     </div>
+
+    </div>
   )
 }
 
